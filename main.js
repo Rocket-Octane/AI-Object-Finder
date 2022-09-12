09f9f8fde0559d8e4cd643f21ddbacfa7774a7df
Rocket-Octane <93807708+Rocket-Octane@users.noreply.github.com>
2022-09-11 22:12:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+objects = [];
+status = "";
+
+function setup(){
+    canvas = createCanvas(400, 300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();    
+}
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    obj_name = document.getElementById("obj_name").value;
+}
+
+function modelLoaded(){
+    console.log("Model is initialized");
+    status = true;
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(video, 0, 0, 400, 300);
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            stroke("red");
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text = objects[i].label;
+            noFill();
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == obj_name){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("object_status").innerHTML = obj_name + " Found";
+                var SpeechRecognition = window.webkitSpeechRecognition;
+                var recognition = new SpeechRecognition();
+                SpeechSynthesisUtterance(obj_name + " found");
+            }
+        }
+    }
+}
